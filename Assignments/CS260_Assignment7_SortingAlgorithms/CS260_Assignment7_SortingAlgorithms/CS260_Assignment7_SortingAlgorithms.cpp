5d5995835945c0349a1c939674a7cb4a3bebffb1
jsullivanp96 <jessie.sully@gmail.com>
2020-04-04 12:10:14 -0700

    Please Review

--- Assignments/CS260_Assignment7_SortingAlgorithms/CS260_Assignment7_SortingAlgorithms/CS260_Assignment7_SortingAlgorithms.cpp
+++ Assignments/CS260_Assignment7_SortingAlgorithms/CS260_Assignment7_SortingAlgorithms/CS260_Assignment7_SortingAlgorithms.cpp
@@ -17,6 +17,35 @@
 using namespace std;
 
 // returns true if memory leak
+bool testMemoryLeak();
+
+// prints array in "<index>=><value>" format separated by spaces.
+template <typename T>
+void printArray(ostream &ostr, const MySortableArray<T> array);
+
+// helper function to parse index and value from input - with index validation. Returns parse fails or if index < 0.
+template <typename T>
+bool parseIndexValue(const string &input, int &index, T &value);
+
+// helper function to parse an int from input
+bool parseIndex(const string &input, int &num);
+
+// runs the UI for the project.
+void runProgram();
+
+int main(int argc, const char * argv[]) {
+  // programmer's identification
+  cout << "Programmer: Jessica Sullivan" << endl;
+  cout << "Programmer's ID: 1282151" << endl;
+  cout << "File: " << __FILE__ << endl;
+  
+  runProgram();
+  testMemoryLeak();
+  
+  return 0;
+}
+
+// returns true if memory leak
 bool testMemoryLeak() {
   if (sNumNodeObjects != 0) {
     cerr << "Num leaked nodes: " << sNumNodeObjects << endl;
@@ -25,6 +54,7 @@ bool testMemoryLeak() {
   return false;
 }
 
+// prints array in "<index>=><value>" format separated by spaces.
 template <typename T>
 void printArray(ostream &ostr, const MySortableArray<T> array) {
   if (!array.isEmpty()) {
@@ -36,20 +66,24 @@ void printArray(ostream &ostr, const MySortableArray<T> array) {
   ostr << endl;
 }
 
-bool parseIndexValue(const string &input, int &index, int &value) {
+// helper function to parse index and value from input - with index validation. Returns parse fails or if index < 0.
+template <typename T>
+bool parseIndexValue(const string &input, int &index, T &value) {
   stringstream ss;
   ss.str(input);
   ss >> index >> value;
   return !ss.fail() && index >= 0;
 }
 
+// helper function to parse an int from input
 bool parseIndex(const string &input, int &num) {
   stringstream ss;
   ss.str(input);
   ss >> num;
-  return !ss.fail();
+  return !ss.fail() && num >= 0;
 }
 
+// runs the UI for the project.
 void runProgram() {
   string input = "";
   int index = 0, value = 0, numEntries = 0;
@@ -66,14 +100,13 @@ void runProgram() {
     }
     getline(cin, input);
   }
-  cout << "Data entered: " << endl;
+  cout << endl << "Data entered: " << endl;
   printArray(cout, dataToSort);
   
   do {
     cout << "Number of entries to sort: ";
     getline(cin, input);
-    parseIndex(input, numEntries);
-    if (numEntries < 0 || cin.fail()) {
+    if (!parseIndex(input, numEntries)) {
       cerr << "Error, please try again." << endl << endl;
     }
   } while (numEntries < 0);
@@ -84,35 +117,13 @@ void runProgram() {
   do {
     cout << endl << "Enter an index to view, enter -1 to exit: ";
     getline(cin, input);
-    if (parseIndex(input, index)) {
-      if (dataToSort.validateIndex(index)) {
-        cout << endl << dataToSort.getEntry(index) << endl;
-      }
+    if (parseIndex(input, index) && dataToSort.validateIndex(index)) {
+      cout << endl << dataToSort.getEntry(index) << endl;
     }
     else {
       if (index != -1) {
-        cerr << "Invalid index, please try again." << endl << endl;
+        cerr << "Invalid index, please try again." << endl;
       }
     }
   } while (index != -1);
 }
-
-int main(int argc, const char * argv[]) {
-  // programmer's identification
-  cout << "Programmer: Jessica Sullivan" << endl;
-  cout << "Programmer's ID: 1282151" << endl;
-  cout << "File: " << __FILE__ << endl;
-  
-  runProgram();
-  testMemoryLeak();
-  
-  
-
-  
-
-  
-  
-  return 0;
-}
-
-
--- Assignments/CS260_Assignment7_SortingAlgorithms/CS260_Assignment7_SortingAlgorithms/MySortableArray.h
+++ Assignments/CS260_Assignment7_SortingAlgorithms/CS260_Assignment7_SortingAlgorithms/MySortableArray.h
@@ -20,13 +20,16 @@ using namespace std;
 // static to keep track of instances to check memory leak. Keeps track of all variants.
 static int sNumNodeObjects = 0;
 
+// Element holds an entry's index and value. This allows for greater control and compensation for holes.
 template <typename T>
 struct Element {
-  Element(){ sNumNodeObjects++; }
-  Element(int index, T value) : mIndex(index), mValue(value) { sNumNodeObjects++; }
-  ~Element() { sNumNodeObjects--; }
+  Element(){ sNumNodeObjects++; } // update sNumNodeObjects
+  Element(int index, T value) : mIndex(index), mValue(value) { sNumNodeObjects++; } // update sNumNodeObjects
+  ~Element() { sNumNodeObjects--; } // update sNumNodeObjects
   
+  // copies member variables
   Element& operator=(const Element& element);
+  // makes streaming easier
   template<typename U>
   friend ostream& operator<<(ostream& ostr, const Element<U>* element);
   
@@ -34,6 +37,7 @@ struct Element {
   int mIndex = 0;
 };
 
+// copies member variables
 template <typename T>
 Element<T>& Element<T>::operator=(const Element<T> &element) {
   mValue = element.mValue;
@@ -41,12 +45,15 @@ Element<T>& Element<T>::operator=(const Element<T> &element) {
   return *this;
 }
 
+// makes streaming easier
 template<typename U>
 ostream& operator<<(ostream& ostr, const Element<U>* element) {
   ostr << element->mIndex << "=>" << element->mValue;
   return ostr;
 }
 
+
+// Sortable array that implements quickSort for sorting
 template <typename T>
 class MySortableArray {
 public:
@@ -58,34 +65,50 @@ public:
   
   MySortableArray& operator=(const MySortableArray& srcArray);
   
-  //inititalizes the array
-  void init() { mMySortableArray = new Element<T>[0]; }
   int getSize() const { return mSize; }
-  //returns desired entry or throws exception
-  const T& getEntry(int index) const;
-  // insertion sort to add entry by index value, overwrites same index
-  void addEntry(int index, T value);
-  //deleteEntry will delete ALL matching entries. The bool is an error catching mechanism False means that the array doesn't exist (null) or its empty or that there wasn't a match
-  bool deleteIndex(int index);
-  bool sort(int numElements);
   bool isEmpty() const { return mSize == 0; }
+  
+  // returns desired entry or throws exception - make sure to validate indexes
+  const T& getEntry(int index) const;
+  
+  // insertion sort to add entry by index value, will overwrite index if index is already in use.
+  void addEntry(int index, T value);
+  
+  // error catching mechanism - returns false if index is not in array.
+  bool deleteIndex(int index);
+  
+  // implements quick sort to sort a certain number of elements - note that it is not index based.
+  bool sort(int numElements);
+  
+  // displays contents of array using "<index>=><value>" format separated by spaces.
   void display(ostream &str) const;
+  
+  // validates that index is in array
   bool validateIndex(int index) const;
   
 private:
   int mSize = 0;
   Element<T> *mMySortableArray = nullptr;
   
-  //helper function to copy all entries in dynamically allocated array returns false if there input array has a size < 0 or size != 0 when MySortableArray = nullptr
+  // inititalizes the array
+  void init() { mMySortableArray = new Element<T>[0]; }
+  
+  // helper function to copy all entries in dynamically allocated array returns false if there input array has a size < 0 or size != 0 when MySortableArray = nullptr
   bool copyData(const MySortableArray &srcArray);
-  //helper function to find the loc of the right index, returns -1 if not found.
+  
+  // helper function to find the loc of the correct index or -1 if mSize = 0.
   int getLoc(int index);
+  
+  // helper function to increase size of the array and leaves a hole for adding an entry.
   void increaseArraySize(int newSize, int gapIndex);
+  
+  // These three helper functions help implements the sort function using QuickSort.
   void quickSort(int firstIndex, int last);
   void swap(int firstIndex, int secondIndex);
   int partition(int low, int high);
 };
 
+//default constructor initializes size to zero and dynamicArray to nullptr
 template <typename T>
 MySortableArray<T>::MySortableArray() {
   mSize = 0;
@@ -171,6 +194,10 @@ void MySortableArray<T>::addEntry(int index, T value) {
   int locAdd = 0;
   if (mSize > 0) {
     locAdd = getLoc(index);
+    if (mMySortableArray[locAdd].mIndex == index) {
+      mMySortableArray[locAdd].mValue = value;
+      return;
+    }
     increaseArraySize(mSize + 1, locAdd);
     mMySortableArray[locAdd].mIndex = index;
     mMySortableArray[locAdd].mValue = value;
